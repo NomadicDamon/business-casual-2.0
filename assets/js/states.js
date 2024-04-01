7fb3d5ca520547db957e2bf17c6237c23f49f598
Damon Hart <45018866+NomadicDamon@users.noreply.github.com>
2024-04-01 13:45:16 -0700

    save before first draft submnission

--- assets/js/states.js
+++ assets/js/states.js
@@ -1,25 +1,33 @@
-// Toggle the 'animated' class when the element enters or exits the viewport
 function handleIntersection(entries, observer) {
-    entries.forEach(entry => {
-      if(entry.isIntersecting) {
-        entry.target.classList.add('animated');
-      } else {
-        entry.target.classList.remove('animated');
-      }
-    });
-  }
-  const observer = new IntersectionObserver(handleIntersection);
-  const targets = document.querySelectorAll('.animate');
-  targets.forEach(target => observer.observe(target));  
-
-  //toggle mobile menu states
-  function toggleMobileMenu() {
-    document.querySelector('body').classList.toggle('mobile-menu-active');
-  }
-  document.addEventListener('keydown', function(event) {
-    if (event.key === 'Escape') {
-      toggleMobileMenu()
+  entries.forEach((entry) => {
+    // Toggle 'animated' class based on intersecting status
+    if (entry.isIntersecting) {
+      entry.target.classList.add("animated");
+    } else {
+      entry.target.classList.remove("animated");
     }
   });
+}
 
-  
+// Configure the IntersectionObserver instance
+const observerOptions = {
+  root: null, // observing relative to viewport; can be changed to a specific element
+  rootMargin: "0px",
+  threshold: [0.2, 0.8], // Callback is called when 10% of the item is visible and again when 90% of it goes out of view
+};
+
+const observer = new IntersectionObserver(handleIntersection, observerOptions);
+const targets = document.querySelectorAll(".animate");
+
+// Observe each target
+targets.forEach((target) => observer.observe(target));
+
+//toggle mobile menu states
+function toggleMobileMenu() {
+  document.querySelector("body").classList.toggle("mobile-menu-active");
+}
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    toggleMobileMenu();
+  }
+});
